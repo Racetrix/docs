d82de15731e85a8573f60237dbd16f85a977ffce
Lition802 <2959435045@qq.com>
2026-01-08 11:46:39 +0800

    fix hash webhistory

--- src/router/index.js
+++ src/router/index.js
@@ -1,4 +1,4 @@
-import { createRouter, createWebHistory } from 'vue-router'
+import { createRouter, createWebHistory, createWebHashHistory } from 'vue-router'
 
 // 引入页面组件
 import HomeView from '../views/HomeView.vue'
@@ -47,7 +47,7 @@ const routes = [
 ]
 
 const router = createRouter({
-    history: createWebHistory(import.meta.env.BASE_URL),
+    history: createWebHashHistory(import.meta.env.BASE_URL),
     routes
 })
 
